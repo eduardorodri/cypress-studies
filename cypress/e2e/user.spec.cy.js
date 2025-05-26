510c07f7b2336da35337755e1738bb7033136c82
eduardorodri <eduardo-94rodrigues@outlook.com>
2025-05-26 11:58:34 -0400

    Added new user tests

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -9,7 +9,13 @@ describe('Orange HRM tests', () => {
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredencialAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']"
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    gerenicField: ".oxd-input--active",
+    gerenicDate: "[placeholder='yyyy-mm-dd']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
   }
 
 
@@ -21,7 +27,21 @@ describe('Orange HRM tests', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
     cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type("FirstNameTest")
+    cy.get(selectorList.lastNameField).clear().type("LastNameTest")
+    cy.get(selectorList.gerenicField).eq(3).clear().type("Nickname teste")
+    cy.get(selectorList.gerenicField).eq(4).clear().type("9070")
+    cy.get(selectorList.gerenicField).eq(5).clear().type("2437")
+    cy.get(selectorList.gerenicField).eq(6).clear().type("17009118")
 
+    cy.get(selectorList.gerenicDate).eq(0).clear().type("2010-01-01")
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.gerenicDate).eq(1).clear().type("2005-06-14")
+    cy.get(selectorList.dateCloseButton).click()
+
+    cy.get(selectorList.submitButton).eq(0).click()
+    //cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
 
   it('Login - Fail', () => {
@@ -30,6 +50,5 @@ describe('Orange HRM tests', () => {
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredencialAlert)
-
   })
 })
