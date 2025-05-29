0ccca2fdf236f002a2d86d6843bcd76624176ad6
eduardorodri <eduardo-94rodrigues@outlook.com>
2025-05-29 10:04:55 -0400

    atualização de userspec

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -13,9 +13,14 @@ describe('Orange HRM tests', () => {
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     gerenicField: ".oxd-input--active",
-    gerenicDate: "[placeholder='yyyy-mm-dd']",
+    gerenicDate: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
-    submitButton: "[type='submit']"
+    submitButton: "[type='submit']",
+    selectField: "[tabindex='0']",
+    firstComboBox: ".oxd-select-dropdown > :nth-child(9)",
+    secondComboBox: ".oxd-select-dropdown > :nth-child(4)",
+    thirdComboBox: ".oxd-select-dropdown > :nth-child(7)",
+    selectBox: ".oxd-radio-input"
   }
 
 
@@ -29,18 +34,26 @@ describe('Orange HRM tests', () => {
     cy.get(selectorList.myInfoButton).click()
     cy.get(selectorList.firstNameField).clear().type("FirstNameTest")
     cy.get(selectorList.lastNameField).clear().type("LastNameTest")
-    cy.get(selectorList.gerenicField).eq(3).clear().type("Nickname teste")
+    //cy.get(selectorList.gerenicField).eq(3).clear().type("Nickname")
     cy.get(selectorList.gerenicField).eq(4).clear().type("9070")
     cy.get(selectorList.gerenicField).eq(5).clear().type("2437")
     cy.get(selectorList.gerenicField).eq(6).clear().type("17009118")
 
-    cy.get(selectorList.gerenicDate).eq(0).clear().type("2010-01-01")
+    cy.get(selectorList.gerenicDate).eq(0).clear().type('2020-07-13')
     cy.get(selectorList.dateCloseButton).click()
     cy.get(selectorList.gerenicDate).eq(1).clear().type("2005-06-14")
     cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.selectBox).eq(1).click()
+
+    cy.get(selectorList.selectField).eq(0).click()
+    cy.get(selectorList.firstComboBox).click()
+    cy.get(selectorList.selectField).eq(1).click()
+    cy.get(selectorList.secondComboBox).click()
+    cy.get(selectorList.selectField).eq(2).click()
+    cy.get(selectorList.thirdComboBox).click()
 
     cy.get(selectorList.submitButton).eq(0).click()
-    //cy.get('body').should('contain', 'Successfully Updated') - Funcionara de ambas as formas, essa e a de baixo!
+    cy.get('body').should('contain', 'Successfully Updated') //- Funcionara de ambas as formas, essa e a de baixo!
     cy.get('.oxd-toast-close')
   })
 
