19620c69bc12bf0ca9729c49d44bf87081de2dd4
eduardorodri <eduardo-94rodrigues@outlook.com>
2025-06-01 12:48:00 -0400

    Adicionado dados randomicos em userspec utilizando dayjs e chancejs

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,21 @@
+import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+
+describe('Login Orange HRM tests', () => {
+
+  it('Login - Fail', () => {
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    loginPage.checkLoginFail()
+  })
+
+  it('Login - Success', () => {
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
+    dashboardPage.checkDashboardPage()
+  })
+})
--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,27 +4,32 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+import dayjs from 'dayjs'
+const Chance = require('chance');
+
+const chance = new Chance();
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
+
+
+
 describe('Orange HRM tests', () => {
 
   it('User Info Update - Success', () => {
+    const dataDeInicio = dayjs(chance.date({ year: 2024 })).format('YYYY-DD-MM')
+    const dataDeNascimento = dayjs(chance.birthday({ type: 'adult' })).format('YYYY-DD-MM')
+
     loginPage.acessLoginPage()
     loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
     dashboardPage.checkDashboardPage()
     menuPage.acessMenu()
-    myInfoPage.personalDetails('Cypress', 'Test', '3024', '123456', '67890')
-    myInfoPage.dataDetails('2023-21-05', '1990-05-12')
+    myInfoPage.personalDetails(chance.first(), chance.last(), chance.bb_pin(), chance.fbid(), chance.ssn())
+    myInfoPage.dataDetails(dataDeInicio, dataDeNascimento)
     myInfoPage.selectComboBox()
     myInfoPage.genderCheck()
     myInfoPage.saveButton()
   })
 
-  it('Login - Fail', () => {
-    loginPage.acessLoginPage()
-    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
-    loginPage.checkLoginFail()
-  })
 })
